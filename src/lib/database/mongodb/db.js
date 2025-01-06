7e0eabc86bf8a7799cc346ff82aaf9ebbcd9da09
MatteoRuffa <cortese_17@protonmail.com>
2025-01-06 15:50:04 +0100

    some db fix_2

--- src/lib/database/mongodb/db.js
+++ src/lib/database/mongodb/db.js
@@ -1,15 +1,8 @@
-"use strict";
-
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/bool_n_dreams', {
-      // Usa il nuovo parser URL per compatibilità con MongoDB moderno.
-      useNewUrlParser: true,
-      // Migliora la gestione della connessione.
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect('mongodb://127.0.0.1:27017/bool_n_dreams');
     console.log('MongoDB connesso con successo!');
   } catch (err) {
     console.error(`Errore di connessione: ${err.message}`);
