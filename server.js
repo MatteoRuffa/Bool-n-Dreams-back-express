6187172b620d8bd7b612735d4c7f13ac02a3858d
MatteoRuffa <cortese_17@protonmail.com>
2025-01-06 16:09:54 +0100

    some db fix_3

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const path = require('path');
 const connectDB = require(path.join(__dirname, 'src', 'lib', 'database', 'mongodb', 'db'));
+const mongoose = require('mongoose');
 
 
 const app = express();
@@ -13,6 +14,30 @@ connectDB();
 // Middleware e route
 app.use(express.json());
 
+//Schema di esempio per testare la connessione
+const TestSchema = new mongoose.Schema({
+  name: { type: String, required:true },
+  age: { type: Number },
+})
+
+const TestModel = mongoose.model('Test', TestSchema);
+
+//proviamo un test di inserimento
+app.get('/addTestData', async (req, res) => {
+  try {
+    const newTestData = new TestModel({
+      name: 'Matteo',
+      age: 32,
+    });
+
+    await newTestData.save();
+    res.send('dati inseriti con successo!!!');
+  } catch (err) {
+    res.status(500).send('Errore nell\'inserimento dei dati:' + err.message);
+  }
+});
+
+//rotta principale
 app.get('/', (req, res) => res.send('API funzionante!'));
 
 const PORT = 5000;
